487614a284d0726ed10257e56be83b3b302ac1fb
zhanggi3 <gio.zhang@oocl.com>
2019-08-17 14:47:37 +0800

    feat,test:sshould_return_TIE_when_Player1_HighCard_Equal_Player2

--- src/test/java/com/gio/PokeHands/CompareCardTest.java
+++ src/test/java/com/gio/PokeHands/CompareCardTest.java
@@ -33,4 +33,26 @@ public class CompareCardTest {
     String result = compareCard.judgeTwoPlayerPoke(player1,player2);
     Assert.assertEquals(PLAYER_1_WIN,result);
   }
+  @Test
+  public void should_return_TIE_when_Player1_HighCard_Equal_Player2(){
+    List<Card> player1 = new ArrayList<Card>(){{
+      add(new Card("H","2"));
+      add(new Card("D","3"));
+      add(new Card("S","5"));
+      add(new Card("C","9"));
+      add(new Card("D","A"));
+    }};
+    List<Card> player2 = new ArrayList<Card>(){{
+      add(new Card("H","2"));
+      add(new Card("S","3"));
+      add(new Card("D","5"));
+      add(new Card("S","9"));
+      add(new Card("C","A"));
+    }};
+
+    String result1 = new Card("S","A").getNumber();
+    CompareCard compareCard = new CompareCard();
+    String result = compareCard.judgeTwoPlayerPoke(player1,player2);
+    Assert.assertEquals(TIE,result);
+  }
 }
